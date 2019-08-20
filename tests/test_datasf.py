6291eb385e4a0597a18cbc47e36f00e6b7b5e290
Hui Shao <huiran.shao@sfgov.org>
2019-08-20 11:04:51 -0700

    unit test for datasf filter

--- tests/test_datasf.py
+++ tests/test_datasf.py
@@ -1,13 +1,29 @@
 """Tests for Records resource"""
+import json
+from unittest.mock import patch
 from service.models.datasf import DataSF
 
+# pylint: disable=line-too-long
+MOCK_DATASF_JSON = """[{"title":"180","release_year":"2011","locations":"Epic Roasthouse (399 Embarcadero)","production_company":"SPI Cinemas","director":"Jayendra","writer":"Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba ","actor_1":"Siddarth","actor_2":"Nithya Menon","actor_3":"Priya Anand"},{"title":"180","release_year":"2011","locations":"Mason & California Streets (Nob Hill)","production_company":"SPI Cinemas","director":"Jayendra","writer":"Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba ","actor_1":"Siddarth","actor_2":"Nithya Menon","actor_3":"Priya Anand"},{"title":"180","release_year":"2011","locations":"Justin Herman Plaza","production_company":"SPI Cinemas","director":"Jayendra","writer":"Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba ","actor_1":"Siddarth","actor_2":"Nithya Menon","actor_3":"Priya Anand"}]"""
+
 def test_datasf_filter():
+    """ Test DataSF filter with mock response """
+    datasf = DataSF()
+    with patch('service.models.datasf.requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = json.loads(MOCK_DATASF_JSON)
+        response = datasf.filter('https://data.sfgov.org/resource/wwmu-gmzc.json')
+
+    assert response
+    assert isinstance(response, list)
+    assert len(response) == len(json.loads(MOCK_DATASF_JSON))
+
+def test_datasf_filter_e2e():
     """ Test DataSF filter """
     datasf = DataSF()
     response = datasf.filter('https://data.sfgov.org/resource/wwmu-gmzc.json')
     assert response
     assert isinstance(response, list)
-
 
 def test_datasf_filter_error():
     """ Test DataSF filter error state """
