98e79d740b4f29fabb45b9a8cb7ca0212404b505
hshaosf <huiran.shao@sfgov.org>
2019-10-16 12:41:49 -0700

    Refactor sf-arcgis-sdk and minor update (#6)
    * refactor to use sf-arcgis-sdk
    
    * update records to default values when field requested

--- service/models/record.py
+++ service/models/record.py
@@ -5,12 +5,17 @@ from .datasf import DataSF
 class Record():
     """Record class"""
     data = {}
+    # possible field options
+    field_options = ['year_built', 'building_area']
 
     def __init__(self, parcel):
         self.data = {'parcel': parcel}
 
     def get(self, fields_array):
         """ Get method """
+        # setting default null values for possible field options
+        for field in set(self.field_options) & set(fields_array):
+            self.data[field] = None
         if 'year_built' in fields_array or 'building_area' in fields_array:
             tax_roll = self.get_asr_tax_roll()
             if 'year_built' in fields_array and 'year_property_built' in tax_roll:
--- service/resources/arcgis.py
+++ service/resources/arcgis.py
@@ -1,12 +1,9 @@
 """ArcGIS module"""
 import os
-import math
 import json
-import urllib
-import requests
 import falcon
 import jsend
-import usaddress
+from sf_arcgis_sdk.sf_arcgis import SfArcgis
 
 class Arcgis():
     """Welcome class"""
@@ -35,66 +32,19 @@ class Arcgis():
 
         response = {'message': 'parcels'}
         if 'address' in req.params:
-            addr = usaddress.tag(req.params['address'])
+            address = req.params['address']
             options = {'returnGeometry':False, 'returnSuggestions':False}
             if 'returnSuggestions' in req.params and req.params['returnSuggestions'] == 'true':
                 options['returnSuggestions'] = True
             if 'returnGeometry' in req.params:
                 options['returnGeometry'] = req.params['returnGeometry']
-            parcels = self.pln_parcel(addr, options)
+
+            sfarcgis = SfArcgis()
+            sfarcgis.set_layer('parcel', os.environ.get('PLN_ARCGIS_PARCEL'))
+            parcels = sfarcgis.get_fields_by_address(address, options)
             response = {'parcels': parcels}
         resp.body = json.dumps(jsend.success(response))
         resp.status = falcon.HTTP_200
-
-    def pln_parcel(self, addr, options=None):
-        """ get parcels from Planning ArcGIS """
-        parcels = {}
-        url = urllib.parse.urljoin(os.environ.get('PLN_ARCGIS_PARCEL')+'/', 'query')
-        params = {'outFields':'blklot,ADDRESS', 'returnGeometry':'false', 'f':'json'}
-        if options['returnGeometry']:
-            params['returnGeometry'] = options['returnGeometry']
-        if 'AddressNumber' in addr[0] and 'StreetName' in addr[0]:
-            where = "base_address_num="+ addr[0]['AddressNumber']
-            where += " and street_name='"+addr[0]['StreetName'].upper()+"'"
-            if 'OccupancyIdentifier' in addr[0]:
-                where += " and unit_address='"+addr[0]['OccupancyIdentifier'].upper()+"'"
-            params['where'] = where
-            response = self.query(url, params)
-            if response and 'features' in response and response['features']:
-                parcels = response['features']
-            elif response is not None and options['returnSuggestions']:
-                # auto suggestions
-                if 'OccupancyIdentifier' in addr[0]:
-                    where = "base_address_num="+ addr[0]['AddressNumber']
-                    where += " and street_name='"+addr[0]['StreetName'].upper()+"'"
-                    params['where'] = where
-                    response = self.query(url, params)
-                    if response and 'features' in response and response['features']:
-                        parcels = response['features']
-                if not parcels:
-                    base_num = math.floor(int(addr[0]['AddressNumber'])/100)*100
-                    where = "base_address_num >=" + str(base_num)
-                    where += " and base_address_num <"+str(base_num+100)
-                    where += " and street_name='"+addr[0]['StreetName'].upper()+"'"
-                    params['where'] = where
-                    response = self.query(url, params)
-                    if response and 'features' in response and response['features']:
-                        parcels = response['features']
-        return parcels
-
-    @staticmethod
-    def query(url, params):
-        """ Queries an url """
-        response = {}
-        headers = {}
-        try:
-            request = requests.get(url, params=params, headers=headers)
-            if request.status_code == 200:
-                response = request.json()
-            return response
-        # pylint: disable=bare-except
-        except:
-            return None
 
     @staticmethod
     def has_missing_env(env_list):
