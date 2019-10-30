d297daa4091a5a630c65c998b05e7d1de3a7760c
Hui Shao <huiran.shao@sfgov.org>
2019-10-30 16:22:24 -0700

    refractor retrieval for datasf data

--- service/models/record.py
+++ service/models/record.py
@@ -1,33 +1,53 @@
 """Record model"""
 import os
+from sodapy import Socrata
 from sf_arcgis_sdk.sf_arcgis import SfArcgis
-from .datasf import DataSF
 
 class Record():
     """Record class"""
-    # possible field options
-    field_options = ['year_built', 'building_area']
+    # possible field options from DataSF
+    field_datasf_options = {
+        'year_built' : {'source': 'datasf_asr_tax_roll', 'field': 'year_property_built'},
+        'building_area' : {'source': 'datasf_asr_tax_roll', 'field': 'property_area'},
+        'ceqacode' : {'source': 'datasf_historic_ceqa', 'field': 'ceqacode'}
+    }
+    datasf_source_opts = {
+        'datasf_asr_tax_roll' : {
+            'parcel_field_name': 'parcel_number',
+            'order':'closed_roll_year DESC'
+            },
+        'datasf_historic_ceqa' : {
+            'parcel_field_name': 'APN'
+            }
+    }
     # possible parcel field options
     field_parcel_options = ['blklot', 'block_num', 'lot_num', 'address']
 
+
     def __init__(self, parcel_num):
-        self.data = {}
+        # parcel number associated with this record
         self.parcel_num = parcel_num
+        # data object associated with this record
+        self.data = {}
 
     def get(self, fields_array):
         """ Get method """
-        # setting default null values for possible field options
-        for field in set(self.field_options) & set(fields_array):
+
+        # Retrieving for DataSF fields
+        datasf_sources = {}
+        for field in set(self.field_datasf_options.keys()) & set(fields_array):
+            # setting default null values for possible field options
             self.data[field] = None
+            # build DataSF sources array
+            field_opt = self.field_datasf_options[field]
+            if field_opt['source'] not in datasf_sources.keys():
+                datasf_sources[field_opt['source']] = {}
+            datasf_sources[field_opt['source']][field] = field_opt
 
-        if 'year_built' in fields_array or 'building_area' in fields_array:
-            tax_roll = self.get_asr_tax_roll()
-            if 'year_built' in fields_array and 'year_property_built' in tax_roll:
-                self.data['year_built'] = tax_roll['year_property_built']
-            if 'building_area' in fields_array and 'property_area' in tax_roll:
-                self.data['building_area'] = tax_roll['property_area']
+        for (source_name, fields) in datasf_sources.items():
+            self.data.update(self.get_datasf_data(source_name, fields))
 
-        # if retrieving for parcel field ata
+        # Retrieving for parcel fields
         field_parcel_request = set(self.field_parcel_options) & set(fields_array)
         if field_parcel_request:
             self.data['parcels'] = []
@@ -41,14 +61,21 @@ class Record():
 
         return self.data
 
-    def get_asr_tax_roll(self):
-        """ Get data from Assessor Property Tax Roll"""
-        response = DataSF().filter(
-            os.environ.get('DATASF_ASR_TAX_ROLL'),
-            {'parcel_number': self.parcel_num, '$order':'closed_roll_year DESC'})
-        if response and isinstance(response, list):
-            return response[0]
-        return {}
+    def get_datasf_data(self, source_name, fields):
+        """ Get data from DataSF """
+        data = {}
+        client = Socrata(os.environ.get('DATASF_DOMAIN'), None)
+        source_id = os.environ.get(source_name.upper()+'_ID')
+        if source_id:
+            datasf_source_opt = self.datasf_source_opts[source_name]
+            order = datasf_source_opt.get('order', '')
+            where = datasf_source_opt.get('parcel_field_name') + "='"+self.parcel_num+"'"
+            response = client.get(source_id, where=where, order=order)
+            if response and isinstance(response, list):
+                row = response[0]
+                for (field, field_opt) in fields.items():
+                    data[field] = row.get(field_opt['field'], None)
+        return data
 
     @staticmethod
     def normalize_parcel_field_names(parcel_object):
